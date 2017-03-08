d464f2c69218ddb586e500bd9135c5120c1dafa9
Jakub Sękowski <sequba@gmail.com>
2017-03-08 22:59:02 +0100

    Working on problem 1: comprehensive preprocessing
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

---
+++ p1/synonyms.py
@@ -0,0 +1,107 @@
+'''
+gunzip -c ../../data/poczatki_wikipediowe.txt.gz | head -n 10000 | python3 my_tokenize.py | python3 synonyms.py | sort -R | less -r
+
+gunzip -c ../../data/poczatki_wikipediowe.txt.gz | pv -l -s 3024407 | python3 my_tokenize.py | python3 synonyms.py
+'''
+
+from sys import stdin
+import unicodedata as ud
+import re
+
+def highlight(string):
+    bold = '\033[1m'
+    w  = '\033[0m'  # white (normal)
+    r  = '\033[31m' # red
+    g  = '\033[32m' # green
+    o  = '\033[33m' # orange
+    b  = '\033[34m' # blue
+    p  = '\033[35m' # purple
+    #return string
+    return bold+g+' '+string+' '+w
+
+comment_regex = re.compile('! --((?!--).)*--')
+open_comment_regex = re.compile('! --((?!--).)*$')
+category_regex = re.compile('= = =((?!= = =).)*= = =')
+open_category_regex = re.compile('= = =((?!= = =).)*$')
+
+def remove_occurences(regex_object, string):
+    m = regex_object.search(string)
+    while m:
+        string = regex_object.sub('', string)
+        m = regex_object.search(string)
+    return string
+
+
+def prepare(line):
+    unicode_dashes = {173, 1418, 1470, 8208, 8209, 8210, 8211, 8212, 8213, 8722, 65123, 65293}
+    translation_table = dict.fromkeys(unicode_dashes, '-')
+
+    line = line.strip()
+    line = line.translate(translation_table).replace('& amp ; ndash ;', '-')
+    line = remove_occurences(comment_regex, line)
+    line = remove_occurences(open_comment_regex, line)
+    line = remove_occurences(category_regex, line)
+    line = remove_occurences(open_category_regex, line)
+
+    return line
+
+def suitable(string):
+    conditions = [
+        # empty lines
+        string == '',
+        # infoboxes
+        string.count('|') > 3,
+        # podobne dziwy
+        string.count('br /') > 3,
+        # redirection pages
+        string.startswith('/ > # redirect')# redirect
+    ]
+
+    if any(conditions):
+        return False
+    return True
+    
+definition_pattern = '^((?!{0}).)*({0}).*$'
+defining_words = [ ' - ', ' jest ', ' to ' ]
+regexes = [re.compile(definition_pattern.format(w)) for w in defining_words]
+
+def before_definition(string):
+    string = ' ' + string
+    matches = [ m.span(2) for pattern in regexes for m in [pattern.match(string)] if m ]
+    if matches:
+        (start, end) = min(matches)
+        print(string[:start] + highlight(string[start:end]) + string[end:end+50])
+        return string[:start]
+    else:
+        return None
+
+matched = 0
+unmatched = 0
+rejected = 0
+for line in stdin:
+    line = prepare(line)
+
+    if line[0:3] == '###': # title line
+        title = line[3:]
+        continue
+    elif not suitable(line): # garbage
+        rejected += 1
+        continue
+    
+    term = before_definition(line)
+    if not term: # bad structure
+        unmatched += 1
+        continue
+
+    matched += 1
+    # proper definition (presumably)
+    # print('{0} ### {1}'.format(title, term))
+
+
+# print('{0} vs {1} + {2}'.format(matched, unmatched, rejected))
+'''
+matched:     625398
+unmatched:    94177
+rejected:   1296689
+'''
+
