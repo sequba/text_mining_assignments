05499d137b9f969764e13549626d4d7c60830578
Jakub Sękowski <sequba@gmail.com>
2017-03-12 12:52:24 +0100

    1.1 done, awfully slow, optimization necessary
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

--- p1/synonyms.py
+++ p1/synonyms.py
@@ -16,7 +16,6 @@ def highlight(string):
     o  = '\033[33m' # orange
     b  = '\033[34m' # blue
     p  = '\033[35m' # purple
-    #return string
     return bold+g+' '+string+' '+w
 
 comment_regex = re.compile('! --((?!--).)*-- ')
@@ -45,6 +44,7 @@ def prepare(line):
     line = remove_occurences(open_category_regex, line)
     line = remove_occurences(subcategory_regex, line)
     line = remove_occurences(open_subcategory_regex, line)
+    line = line.replace('right ', '')
 
     return line
 
@@ -57,19 +57,15 @@ def suitable(string):
         # podobne dziwy
         string.count('br /') > 3,
         # redirection pages
-        string.startswith('/ > # redirect')# redirect
+        string.startswith('/ > # redirect')
     ]
 
     if any(conditions):
         return False
     return True
-    
-# definition_pattern = '^((?!{0}).)*({0}).*$'
-# defining word outside parathesis (sort of)
-# TODO: whats wrong with nested stars?
-# definition_pattern = '^'+'('+'((?!{0})[^\(])*'+'(\([^\)]*\))*'+')*'+'(?P<word>{0})'
 
-def_pattern = '^'+'('+'((?!{0})[^(])*'+'(\([^)]*\))*'+'((?!{0})[^(])*'+')'+'(?P<word>{0})'
+# first occurence of a def_word outside paranthesis (nested * not working, so its an approximation)
+def_pattern = '^'+'((?!{0})[^(])*'+'(\([^)]*\))*'+'((?!{0})[^(])*'+'(\([^)]*\))*'+'((?!{0})[^(])*'+'(?P<word>{0})'
 def_words = [ ' - ', ' jest ', ' to ' ]
 def_regexes = [re.compile(def_pattern.format(w)) for w in def_words]
 
@@ -83,23 +79,42 @@ def split_definition(string):
     else:
         return ('', string)
 
-#syn_pattern = "{0}"+" *"+"("+"({1}) *"+")*"+"(?P<word>'''*[^']*'''*)" #+"("+" *, *"+"(''+[^']*''+)"+")*"
-syn_pattern = "{0}"+" *"+"("+"({1}) *"+")*"+"(?P<word>"+"'''*[^']*'''*" + "("+" *, *"+"'''*[^']*'''*"+")*"+")"
+# an intro_word followed by a series of ign_words indicate the beginning of interesting fragment from which the synonyms are to be extracted
+schema = "(?P<word>"+"'''*[^']*'''*" + "("+" *, *"+"'''*[^']*'''*"+")*"+")"
+syn_pattern = "{0}"+" *"+"("+"({1}) *"+")*"+schema
+body_syn_pattern = "^ *"+"(({0}) *"+"("+"({1}) *"+")*"+")?"+schema
 common_words = [ 'też', 'inaczej', 'właściwie', 'właść .', 'właśc .', 'oryginalnie', 'w oryginale', 'dawniej', 'dokładnie', 'dokładniej', 'zwyczajowo', 'zwycz .', 'potocznie', 'pot .', 'potocz .', 'syn.', 'synonimicznie', 'także', 'również', 'krótko', 'krócej', 'w skrócie', 'prościej', 'zwyczajnie', 'rzadziej', 'czasem', 'czasami', 'precyzyjnie', 'precyzyjniej', 'zwany', 'zwana', 'zwane', 'zwani', 'nazywany', 'nazywana', 'nazywane', 'nazywani', 'znany', 'znana', 'znane', 'tzw .', 'często', 'częściej', 'określany', 'określana', 'określane', 'określani']
 intro_words = [ ' lub ', ' albo ', ' skrót ', ' synonim ', ' synonimy ', ' czyli ', '^', '\( '] + [ ' '+w+' ' for w in common_words ]
 ign_words = [ 'jako ', 'nazwą ', 'pod nazwą ', 'mianem ', 'terminem ', 'określeniem ', 'pod określeniem ', ': ', '- ', 'po prostu ' ] + [ w+' ' for w in common_words ]
 ign_alternative = '|'.join(ign_words)
 syn_regexes = [ re.compile(syn_pattern.format(w, ign_alternative)) for w in intro_words ]
+intro_alternative = '|'.join(intro_words)
+body_syn_regex = re.compile(body_syn_pattern.format(intro_alternative, ign_alternative))
+
+quoted_regex = re.compile("'''*[^']*'''*")
 
 def extract_synonyms(head, body):
-    string = head
-    matches = [ m.group('word') for r in syn_regexes for m in r.finditer(string) ]
-    # len...
-    for m in matches:
-        string = string.replace(m, highlight(m))
-    if matches:
+    interesting_substrings = { m.group('word') for r in syn_regexes for m in r.finditer(head) }
+    interesting_substrings_in_body = { m.group('word') for m in body_syn_regex.finditer(' '+body) }
+    interesting = interesting_substrings | interesting_substrings_in_body
+
+    synonyms = set()
+    for s in interesting:
+        quoted = quoted_regex.finditer(s)
+        synonyms = { inner for q in quoted for inner in [q.group().strip().strip("'").strip()] if len(inner) < 64 }
+    
+    '''
+    if interesting_substrings_in_body:
+        string = head
+        for m in interesting_substrings:
+            string = string.replace(m, highlight(m))
+        print('###', string)
+        string = body
+        for m in interesting_substrings_in_body:
+            string = string.replace(m, highlight(m))
         print('$$$ ', string)
-    return matches
+    '''
+    return synonyms
 
 matched = 0
 unmatched = 0
@@ -108,12 +123,12 @@ for line in stdin:
     line = prepare(line)
 
     if line[0:3] == '###': # title line
-        title = line[3:]
+        title = line[4:]
         continue
-    elif not suitable(line): # garbage
+    elif not suitable(line) or title.endswith('( ujednoznacznienie )'): # garbage
         rejected += 1
         continue
-    
+   
     (head, body) = split_definition(line)
     if head:
         matched += 1
@@ -121,15 +136,10 @@ for line in stdin:
         unmatched += 1
   
     synonyms = extract_synonyms(head, body)
-
-    # proper definition (presumably)
-    # print('{0} ### {1}'.format(title, term))
+    synonyms.discard(title)
+    if synonyms:
+        print('{0} ## {1}'.format(title, ' # '.join(synonyms)))
 
 
 # print('{0} vs {1} + {2}'.format(matched, unmatched, rejected))
-'''
-matched:     625398
-unmatched:    94177
-rejected:   1296689
-'''
 
