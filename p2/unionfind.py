34633502780a25e4c23e2cacd1650c63856e4c4c
Jakub Sękowski <sequba@gmail.com>
2017-04-02 13:43:09 +0200

    union-find rewritten to suit my needs better
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

--- p2/unionfind.py
+++ p2/unionfind.py
@@ -1,75 +1,30 @@
-# Downloaded from: http://code.activestate.com/recipes/577225-union-find/
+class UnionFind:
+    parent = None
+    rank = None
 
-"""
-MakeSet(x) initializes disjoint set for object x
-Find(x) returns representative object of the set containing x
-Union(x,y) makes two sets containing x and y respectively into one set
+    def __init__(self):
+        self.parent = dict()
+        self.rank = dict()
 
-Some Applications:
-- Kruskal's algorithm for finding minimal spanning trees
-- Finding connected components in graphs
-- Finding connected components in images (binary)
-"""
+    def make_set(self, x):
+        self.parent[x] = x
+        self.rank[x]   = 0
 
-def MakeSet(x):
-     x.parent = x
-     x.rank   = 0
+    def union(self, x, y):
+        xRoot = self.find(x)
+        yRoot = self.find(y)
+        if self.rank[xRoot] > self.rank[yRoot]:
+            self.parent[yRoot] = xRoot
+        elif self.rank[xRoot] < self.rank[yRoot]:
+            self.parent[xRoot] = yRoot
+        elif xRoot != yRoot: # Unless x and y are already in same set, merge them
+            self.parent[yRoot] = xRoot
+            self.rank[xRoot] += 1
 
-def Union(x, y):
-     xRoot = Find(x)
-     yRoot = Find(y)
-     if xRoot.rank > yRoot.rank:
-         yRoot.parent = xRoot
-     elif xRoot.rank < yRoot.rank:
-         xRoot.parent = yRoot
-     elif xRoot != yRoot: # Unless x and y are already in same set, merge them
-         yRoot.parent = xRoot
-         xRoot.rank = xRoot.rank + 1
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        else:
+            self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
 
-def Find(x):
-     if x.parent == x:
-        return x
-     else:
-        x.parent = Find(x.parent)
-        return x.parent
-
-# sample code using Union-Find (not needed)
-
-if __name__ == '__main__':
-    import itertools
-
-    class Node:
-        def __init__ (self, label):
-            self.label = label
-        def __str__(self):
-            return self.label
-        
-    l = [Node(ch) for ch in "abcdefg"]      #list of seven objects with distinct labels
-    print("")
-    print("objects labels:\t\t\t", [str(i) for i in l])
-
-    [MakeSet(node) for node in l]       #starting with every object in its own set
-
-    sets =  [str(Find(x)) for x in l]
-    print("set representatives:\t\t", sets)
-    print("number of disjoint sets:\t", len([i for i in itertools.groupby(sets)]))
-
-    assert( Find(l[0]) != Find(l[2]) )
-    Union(l[0],l[2])        #joining first and third
-    assert( Find(l[0]) == Find(l[2]) )
-
-    assert( Find(l[0]) != Find(l[1]) )
-    assert( Find(l[2]) != Find(l[1]) )
-    Union(l[0],l[1])        #joining first and second
-    assert( Find(l[0]) == Find(l[1]) )
-    assert( Find(l[2]) == Find(l[1]) )
-
-    Union(l[-2],l[-1])        #joining last two sets
-    Union(l[-3],l[-1])        #joining last two sets
-
-    sets = [str(Find(x)) for x in l]
-    print("set representatives:\t\t", sets)
-    print("number of disjoint sets:\t", len([i for i in itertools.groupby(sets)]))
-
-    for o in l:
-        del o.parent
